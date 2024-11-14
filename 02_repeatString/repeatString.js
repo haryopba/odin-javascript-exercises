6cfbd8bf7d25938d43710b1dc8109973c30f5bf2
haryopba <bambang@bormadago.com>
2024-11-14 09:28:07 +0700

    solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(text, number_of_repeat) {
+    if(number_of_repeat < 0) {
+        return 'ERROR';
+    }
 
+    let repeatingText = '';
+    for(let i = 0; i < number_of_repeat; i++) {
+        repeatingText += text;
+    }
+    return repeatingText;
 };
 
 // Do not edit below this line
