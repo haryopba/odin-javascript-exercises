7cd95cd7b4ba11b4d0cf1683b6ab109429a6a877
haryopba <bambang@bormadago.com>
2024-11-14 15:59:28 +0700

    solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const yearNow = 2024;
 
+    return people.reduce((oldest, person) => {
+        let oldestAge = (oldest.yearOfDeath !== undefined ? oldest.yearOfDeath : yearNow) - oldest.yearOfBirth; 
+        let personAge = (person.yearOfDeath !== undefined ? person.yearOfDeath : yearNow) - person.yearOfBirth;
+        if(oldestAge > personAge){
+            return oldest;
+        } else {
+            return person;
+        }
+    }, people[0])
 };
 
 // Do not edit below this line
