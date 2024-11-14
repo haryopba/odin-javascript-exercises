52907ea78b854f56aba02c8cd69499cbf61d032f
haryopba <bambang@bormadago.com>
2024-11-14 09:39:29 +0700

    solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...args) {
+    let manipulatedList = [...list]
+    for(let arg of args) {
+        manipulatedList = manipulatedList.filter((item) => item !== arg);
+    }
+    return manipulatedList;
 };
 
 // Do not edit below this line
