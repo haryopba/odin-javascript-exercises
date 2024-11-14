b9a5c377f38e642dfaa8afcfb0fc8a1c09b3240c
haryopba <bambang@bormadago.com>
2024-11-14 15:42:49 +0700

    solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    const banList = '!,.-?+=- '.split('');
+    let sanitezedText = text.toLowerCase();
+    sanitezedText = banList.reduce((workedText, banChar) => workedText.split(banChar).join(''), sanitezedText);
 
+    return sanitezedText.split('').reverse().join('') === sanitezedText;
 };
 
 // Do not edit below this line
